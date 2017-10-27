8f8e9e9f4666f586b5aba7db2ca0bc7d9451e446
David <djlowes@gmail.com>
2017-10-26 23:22:34 -0700

    bed time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,14 @@
-$(document).ready(function() {
-    displayLinks();
-});
-
 //Master object to be used to pull data from
 var mainObject = []
+
 //Ajax call to Govtrack to push into master object
-function displayLinks() {
-  var queryURL = "https://www.govtrack.us/api/v2/role?current=true&limit=541"
+var queryURL = "https://www.govtrack.us/api/v2/role?current=true&limit=541"
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function(response) {
     var people = response.objects;
-    console.log(response.objects)
     //iterate through the object
     for (i=0; i<people.length; i++) {
       //Get image URL of image
@@ -26,7 +21,7 @@ function displayLinks() {
       party = people[i].party;
       gender = people[i].person.gender;
       link = people[i].person.link;
-      image = imageURL
+      image = imageURL;
       nickname = people[i].person.nickname;
       twitterID = people[i].person.twitterid;
       youTubeID = people[i].person.youtubeid;
@@ -39,6 +34,19 @@ function displayLinks() {
       console.log(mainObject["0"].firstName + " " + mainObject["0"].lastName + " from " + mainObject["0"].state)
   });
 
-  console.log("why is this " + mainObject["0"])
-  console.log(mainObject["0"].firstName + " " + mainObject["0"].lastName + " from " + mainObject["0"].state)
-}
+  //console.log("why is this " + mainObject["0"])
+  //console.log(mainObject["0"].firstName + " " + mainObject["0"].lastName + " from " + mainObject["0"].state)
+
+var queryURLTwo = "https://api-us.faceplusplus.com/facepp/v3/face/analyze"
+var key = "gTJWVxi-B7ySv60XLZ1NYsp1lMcpBn6H"
+var secret = "xHV4nyp09d_otLGH4b6fEQHttqVS7gQZ"
+
+$.ajax({
+  url: queryURLTwo + key + secret + $.param({ paramInQuery: 1 }),
+  method: 'POST',
+  data: {
+    paramInBody: 2
+  }
+}).done(function(response) {
+console.log(response)
+});
