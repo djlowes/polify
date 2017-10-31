fc22eca38861d006bee4237c2fd72e4b1c225a22
JPauloBR <jpaulo.araras@gmail.com>
2017-10-31 01:26:37 -0700

    statistics version

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,142 +24,111 @@ var newWindow;
 var searchKeyNoSpaces;
 var searchedFName;
 var searchedLName;
+var congressman = [];
+var data;
+
+var angerLowestToHighest = [];
+var contemptLowestToHighest = [];
+var disgustLowestToHighest = [];
+var fearLowestToHighest = [];
+var happinessLowestToHighest = [];
+var neutralLowestToHighest = [];
+var sadnessLowestToHighest = [];
+var surpriseLowestToHighest = [];
 
 
-databaseRef.on("child_added", function(childSnapshot) {
+$( document ).ready(function() {
 
-  result = childSnapshot.val();
-  // console.log(result);
-  for (var i = result.length - 1; i >= 0; i--) {
-    var name = result[i].firstName+" "+ result[i].lastName;
-    // console.log("name: "+ name);
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("firstName").on("child_added", function(snapshot) {
+    var name = snapshot.val().firstName+" "+ snapshot.val().lastName;
     _results.push(name);
-  }
-  // console.log("Results array: "+ _results);
 
+    var document = window.document,
+        $_window = $( window ),
 
-  var document = window.document,
-      $_window = $( window ),
+          _rEscapeChars = /\/|\\|\.|\||\*|\&|\+|\(|\)|\[|\]|\?|\$|\^/g,
+          _rMatch = /[A-Z]?[a-z]+|[0-9]+/g,
 
-      // _results = [
-      //   'awesome',
-      //   'bakery',
-      //   'creator',
-      //   'doritos',
-      //   'efficient',
-      //   'falafel',
-      //   'great work',
-      //   'high times',
-      //   'idiom',
-      //   'jargon',
-      //   'krill',
-      //   'lazy',
-      //   'masters',
-      //   'nope',
-      //   'opacity',
-      //   'purple rain',
-      //   'quaint',
-      //   'readability',
-      //   'simpleton',
-      //   'terrible',
-      //   'umqua',
-      //   'verocious',
-      //   'waffles',
-      //   'x-rays',
-      //   'yard sale',
-      //   'zappos'
-      // ],
+          _keys = [
+          13,
+          9
+        ],
 
-        _rEscapeChars = /\/|\\|\.|\||\*|\&|\+|\(|\)|\[|\]|\?|\$|\^/g,
-        _rMatch = /[A-Z]?[a-z]+|[0-9]+/g,
+          _length = _results.length,
 
-        _keys = [
-        13,
-        9
-      ],
+          $_result = $( '#result' ),
+          $_search = $( '#search-bar' ),
+          $_searchContainer = $( '#search-container' ),
 
-        _length = _results.length,
+          _resultPlaceholder = $_result.val();
 
-        $_result = $( '#result' ),
-        $_search = $( '#search-bar' ),
-        $_searchContainer = $( '#search-container' ),
+    $_search.on( "keydown", function ( e ) {
 
-        _resultPlaceholder = $_result.val();
+      if ( _keys.indexOf( e.keyCode ) !== -1 ) {
 
-  $_search.on( "keydown", function ( e ) {
+        $_search.val( $_result.val() );
 
-    if ( _keys.indexOf( e.keyCode ) !== -1 ) {
-
-      $_search.val( $_result.val() );
-
-      return false;
-
-    }
-
-  }).on( "keyup", function () {
-
-    var value = $_search.val().replace( _rEscapeChars, "" ),
-        regex = new RegExp( "^"+value, "i" ),
-        matches = [];
-
-
-    if ( value ) {
-
-      for ( var i = _length; i--; ) {
-
-        if ( regex.test( _results[ i ] ) ) {
-
-          matches.push( _results[ i ] );
-
-        } else {
-
-          $_result.val( "" );
-
-        }
+        return false;
 
       }
 
-      if ( matches.length ) {
+    }).on( "keyup", function () {
 
-        for ( var i = matches.length; i--; ) {
+      var value = $_search.val().replace( _rEscapeChars, "" ),
+          regex = new RegExp( "^"+value, "i" ),
+          matches = [];
 
-          $_result.val( matches[ i ] );
+
+      if ( value ) {
+
+        for ( var i = _length; i--; ) {
+
+          if ( regex.test( _results[ i ] ) ) {
+
+            matches.push( _results[ i ] );
+
+          } else {
+
+            $_result.val( "" );
+
+          }
 
         }
 
+        if ( matches.length ) {
+
+          for ( var i = matches.length; i--; ) {
+
+            $_result.val( matches[ i ] );
+
+          }
+
+        }
+
+      } else {
+
+        $_result.val( _resultPlaceholder );
+
       }
 
-    } else {
-
-      $_result.val( _resultPlaceholder );
-
-    }
-
-    searchKey = value;
+      searchKey = value;
 
 
+    });
   });
 });
 
 $("#submit-btn").on("click", function(event){
   event.preventDefault();
   sessionStorage.clear();
-
-  // console.log("result val: "+ searchKey);
   
   var searchedArray = searchKey.split(" ");
   searchedFName = searchedArray[0];
   searchedLName = searchedArray[1];
-  // console.log("Search key no spaces: "+ searchedFName + " " + searchedLName);
 
   sessionStorage.setItem("localFirstName", searchedFName);
   sessionStorage.setItem("localLastName", searchedLName);
   newWindow = window.open("politicianPage.html", "_self");
-
-  // newWindow.onload = function(){
-  //   console.log('renderPPage: '+ sessionStorage.getItem("searchKey"));
-  //   $("#polDiv").text("Test: "+sessionStorage.getItem("searchKey"));
-  // };
 
 });
 
@@ -167,69 +136,147 @@ $("#submit-btn").on("click", function(event){
 function renderPPage(){
   var fName = sessionStorage.getItem("localFirstName");
   var lName = sessionStorage.getItem("localLastName");
-  console.log('renderPPage: '+ sessionStorage.getItem("localFirstName") + " " + sessionStorage.getItem("localLastName")); 
-  $("#polDiv").text("Congressman: "+sessionStorage.getItem('localFirstName') +  " "  + sessionStorage.getItem('localLastName'));
+  $("#pol-info").text("Congressman: "+sessionStorage.getItem('localFirstName') +  " "  + sessionStorage.getItem('localLastName'));
 
-  databaseRef.on("child_added", function(childSnapshot) {
-
-    result = childSnapshot.val();
-    // console.log(result);
-    for (var i = result.length - 1; i >= 0; i--) {
-      if(result[i].firstName == fName && result[i].lastName == lName) {
-        var twitterid = result[i].twitter;
-        twitterFeedByUser(twitterid);
-        console.log("result image: "+ result[i].image);
-        $("#politicianBioImage").attr("src", result[i].image);
-        break;
-      }
-    }    
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("firstName").on("child_added", function(snapshot) {
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      twitterFeedByUser(snapshot.val().twitter);
+      $("#politicianBioImage").attr("src", snapshot.val().image);
+    }
   });
-};
 
-function printing(){
-  console.log("Printing: " +this.firstName + " "+ this.lastName);
-  printAfter(this.firstName, this.lastName);
-  return this.firstname;
-};
 
-function printAfter(firstname, lastname) { 
-  console.log("Print After: " +firstname + " "+ lastname);
-  return this.lastname;
+  //anger
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("anger").on("child_added", function(snapshot) {
+    angerLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = angerLowestToHighest.length - 1; i >= 0; i--) {
+        if (angerLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#anger").text("is the "+ (i-1) + " in the anger ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //contemptLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("contempt").on("child_added", function(snapshot) {
+    contemptLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = contemptLowestToHighest.length - 1; i >= 0; i--) {
+        if (contemptLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#contempt").text("is the "+ (i-1) + " in the contempt ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //disgustLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("disgust").on("child_added", function(snapshot) {
+    disgustLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = disgustLowestToHighest.length - 1; i >= 0; i--) {
+        if (disgustLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#disgust").text("is the "+ (i-1) + " in the disgust ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //fearLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("fear").on("child_added", function(snapshot) {
+    fearLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = fearLowestToHighest.length - 1; i >= 0; i--) {
+        if (fearLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#fear").text("is the "+ (i-1) + " in the fear ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //happinessLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("happiness").on("child_added", function(snapshot) {
+    happinessLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = happinessLowestToHighest.length - 1; i >= 0; i--) {
+        if (happinessLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#happiness").text("is the "+ (i-1) + " in the happiness ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //neutralLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("neutral").on("child_added", function(snapshot) {
+    neutralLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = neutralLowestToHighest.length - 1; i >= 0; i--) {
+        if (neutralLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#neutral").text("is the "+ (i-1) + " in the neutral ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //sadnessLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("sadness").on("child_added", function(snapshot) {
+    sadnessLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = sadnessLowestToHighest.length - 1; i >= 0; i--) {
+        if (sadnessLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#sadness").text("is the "+ (i-1) + " in the sadness ranking"  );
+          break;
+        }
+      }
+    }
+  });
+
+  //surpriseLowestToHighest
+  database.ref('polify/politicians/-Kxja5UAXHiGHxjAgrbs').orderByChild("surprise").on("child_added", function(snapshot) {
+    surpriseLowestToHighest.push(snapshot.val());
+    if(snapshot.val().firstName == fName && snapshot.val().lastName == lName) {
+      for (var i = surpriseLowestToHighest.length - 1; i >= 0; i--) {
+        if (surpriseLowestToHighest[i].twitter == snapshot.val().twitter){
+          $("#surprise").text("is the "+ (i-1) + " in the surprise ranking"  );
+          break;
+        }
+      }
+    }
+  });
 };
 
 function twitterFeedByUser(userID) {
   var funcTwitterID = this.twitter;
-  console.log("calling twitter method: "+userID);
-   var key = "8lout56tMiVPh719vYkz3KfRU";
-   var secret = "Cf4Lg8TwWHOfiPgmnRg5SYJLObLvFIF7b881jUMgPQb54MaCqZ";
-   var bearerToken = "AAAAAAAAAAAAAAAAAAAAADO%2F2wAAAAAAWrrzOYxSECQb7GLoA2eDvkaF5BE%3DQkBQjixouGv7kDleqtNIq2CMYPuLqtzxlbsGacJMQqSahhfn0a";
-   var returnArray;
+  var key = "8lout56tMiVPh719vYkz3KfRU";
+  var secret = "Cf4Lg8TwWHOfiPgmnRg5SYJLObLvFIF7b881jUMgPQb54MaCqZ";
+  var bearerToken = "AAAAAAAAAAAAAAAAAAAAADO%2F2wAAAAAAWrrzOYxSECQb7GLoA2eDvkaF5BE%3DQkBQjixouGv7kDleqtNIq2CMYPuLqtzxlbsGacJMQqSahhfn0a";
+  var returnArray;
 
-   var cb = new Codebird;
-   cb.setConsumerKey(key, secret);
-   cb.setBearerToken(bearerToken);
+  var cb = new Codebird;
+  cb.setConsumerKey(key, secret);
+  cb.setBearerToken(bearerToken);
 
-    cb.__call(
+  cb.__call(
      "statuses_userTimeline",
-     {
+    {
       "screen_name": userID,
       "count": 5
     },
-     function (reply) {
+    function (reply) {
       for (var i = reply.length - 1; i >= 0; i--) {
-          console.log("Inside reply: ", reply[i].text);
-
-
-          var tweetDiv = $("<div class='card-body float-left'>");
-          tweetDiv.append(reply[i].text);
-          $("#tweets").append(tweetDiv);
-
-          returnArray = reply[i].text;
-        }
-     },
-     true // this parameter required
+        var tweetDiv = $("<div class='card-body float-left'>");
+        tweetDiv.append(reply[i].text);
+        $("#tweets").append(tweetDiv);
+      }
+   },
+   true // this parameter required
   );
-    return returnArray;
 };
 
 
@@ -910,54 +957,45 @@ function independentSurprise() {
 //OVERALL RANKINGS FOR EACH EMOTION
 
 // Each array will be used to rank congressman on attributes from lowest to highest
-var angerLowestToHighest = [];
-var contemptLowestToHighest = [];
-var disgustLowestToHighest = [];
-var fearLowestToHighest = [];
-var happinessLowestToHighest = [];
-var neutralLowestToHighest = [];
-var sadnessLowestToHighest = [];
-var surpriseLowestToHighest = [];
 
-Array.prototype.sortBy = function(emotionA) {
-  return this.slice(0).sort(function(a, b) {
-    return (a[emotionA] > b[emotionA]) ? 1 : (a[emotionA] < b[emotionA]) ? -1 : 0;
-  });
-  //Push congressman data to Firebase DB
-  // dataRef.ref().child('Congressman').push(mainObject)
-};
+
+// Array.prototype.sortBy = function(emotionA) {
+//   return this.slice(0).sort(function(a, b) {
+//     return (a[emotionA] > b[emotionA]) ? 1 : (a[emotionA] < b[emotionA]) ? -1 : 0;
+//   });
+// };
 
 
 
-// Push to Arrays
-angerLowestToHighest.push(congressman.sortBy('anger').reverse());
-contemptLowestToHighest.push(congressman.sortBy('contempt'));
-disgustLowestToHighest.push(congressman.sortBy('disgust'));
-fearLowestToHighest.push(congressman.sortBy('fear'));
-happinessLowestToHighest.push(congressman.sortBy('happiness'));
-neutralLowestToHighest.push(congressman.sortBy('neutral'));
-sadnessLowestToHighest.push(congressman.sortBy('sadness'));
-surpriseLowestToHighest.push(congressman.sortBy('surprise'));
+// // Push to Arrays
+// angerLowestToHighest.push(congressman.sortBy('anger').reverse());
+// contemptLowestToHighest.push(congressman.sortBy('contempt'));
+// disgustLowestToHighest.push(congressman.sortBy('disgust'));
+// fearLowestToHighest.push(congressman.sortBy('fear'));
+// happinessLowestToHighest.push(congressman.sortBy('happiness'));
+// neutralLowestToHighest.push(congressman.sortBy('neutral'));
+// sadnessLowestToHighest.push(congressman.sortBy('sadness'));
+// surpriseLowestToHighest.push(congressman.sortBy('surprise'));
 // console.log(angerLowestToHighest)
-//console.log(contemptLowestToHighest)
-//console.log(happinessLowestToHighest)
-//console.log(sadnessLowestToHighest)
+// //console.log(contemptLowestToHighest)
+// //console.log(happinessLowestToHighest)
+// //console.log(sadnessLowestToHighest)
 
 
-function angerSort(a, b) {
-  if (a.anger < b.anger)
-    return -1;
-  if (a.anger > b.anger)
-    return 1;
-  return 0;
-}
+// function angerSort(a, b) {
+//   if (a.anger < b.anger)
+//     return -1;
+//   if (a.anger > b.anger)
+//     return 1;
+//   return 0;
+// }
 
-// console.log(congressman.sortBy(angerSort).reverse());
-//console.log(congressman.sort(angerSort));
+// // console.log(congressman.sortBy(angerSort).reverse());
+// //console.log(congressman.sort(angerSort));
 
 
 
-//INSERT CHARTS BELOW
+// //INSERT CHARTS BELOW
 
 
 var ctx = document.getElementById("myChart");
